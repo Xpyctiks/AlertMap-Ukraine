42cccaf075a0898c0cd4c97d19c388b647db183c
Ruslan Sieriebriannikov <1rusua1@gmail.com>
2026-07-22 11:56:22 +0300

    Changed error reporting of set_state() from all errors to Telegram to only one alert if any errors

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ from datetime import datetime, time
 from pathlib import Path
 from dotenv import load_dotenv
 
-VERSION="1.1.0"
+VERSION="1.1.1"
 HA_URL = HA_TOKEN = API_TOKEN = TELEGRAM_CHATID = TELEGRAM_TOKEN = LED_GROUP_NAME = LED_ENTITY = API_URL = ""
 GREEN = [0, 255, 0]
 RED = [255, 0, 0]
 YELLOW = [255, 255, 0]
 MEMORY_UPDATED = False
 DAY_NIGHT_ENABLED = True
-DAY_NIGHT_FROM = ""
-DAY_NIGHT_TO = ""
+DAY_NIGHT_FROM = 23
+DAY_NIGHT_TO = 7
 DAY_NIGHT_DAYBRIGHTNESS = 255
 DAY_NIGHT_NIGHTBRIGHTNESS = 90
 
@@ -102,11 +102,12 @@ async def send_to_telegram(message: str, subject: str = "__name__", ) -> None:
       logging.error(f"Error while sending message to Telegram: {err}")
 
 def load_config():
+  """Loads the current confifuration from .env file"""
   global HA_URL, HA_TOKEN, API_TOKEN, TELEGRAM_CHATID, TELEGRAM_TOKEN, LED_GROUP_NAME, LED_ENTITY, API_URL
   load_dotenv(verbose=True)
   LED_GROUP_NAME = os.getenv('LED_GROUP_NAME','')
-  TELEGRAM_TOKEN = os.getenv('TELEGRAM_CHATID','')
-  TELEGRAM_CHATID = os.getenv('TELEGRAM_TOKEN','')
+  TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN','')
+  TELEGRAM_CHATID = os.getenv('TELEGRAM_CHATID','')
   API_TOKEN = os.getenv('API_TOKEN','')
   API_URL = os.getenv('API_URL','')
   HA_TOKEN = os.getenv('HA_TOKEN','')
@@ -126,11 +127,12 @@ API_URL = "https://api.alerts.in.ua/v1/iot/active_air_raid_alerts_by_oblast.json
 LED_GROUP_NAME = "alertmap_ukraine_all_lights"
 LED_ENTITY = "alertmap_esp8266"
     """)
-    quit(1)
     if TELEGRAM_TOKEN and TELEGRAM_CHATID:
       asyncio.run(send_to_telegram("ERROR! Some important value like API_TOKEN,HA_TOKEN,HA_URL is not configiured in .env file! Can't proceed...",f"🚒AlarmMap-Ukraine update script:"))
+    quit(1)
 
 def check_state() -> bool:
+  """Checks the state of LEDs because they may be totally off."""
   try:
     global LED_GROUP_NAME
     if LED_GROUP_NAME:
@@ -157,18 +159,18 @@ def check_state() -> bool:
     return False
 
 def get_day_night() -> int:
+  """Gets current time of the day - day or night, to set proper brightness"""
   #if not enabled, just return brightness value of the day
   if not DAY_NIGHT_ENABLED:
     return DAY_NIGHT_DAYBRIGHTNESS
   #if enabled
   now = datetime.now().time()
-  #if there is night
-  if (now >= time(23, 0) or now < time(7, 0)):
+  if (now >= time(DAY_NIGHT_FROM, 0) or now < time(DAY_NIGHT_TO, 0)):
     return DAY_NIGHT_NIGHTBRIGHTNESS
   else:
     return DAY_NIGHT_DAYBRIGHTNESS
 
-def set_state(led_id: str, color: list = [0,0,255]):
+def set_state(led_id: str, color: list = [0,0,255]) -> bool:
   """Makes API requests to HomeAssistant and sets a state of every LED"""
   try:
     url_ha = os.path.join(HA_URL,'api/services/light/turn_on')
@@ -184,14 +186,24 @@ def set_state(led_id: str, color: list = [0,0,255]):
     response = requests.post(url_ha, headers=headers, json=data)
     #logging what we've got for debug purpose if DEBUG is enabled in logger
     logging.info(f"set_state() response from HA: {response.text}")
+    return True
   except Exception as err:
     logging.error(f"set_state() general error: {err}")
-    asyncio.run(send_to_telegram(f"set_state() general error: {err}",f"🚒AlarmMap-Ukraine update script:"))
+    return False
+
+def reset_brightness() -> None:
+  """Deletes memory file at the night start and night end time to set all LEDs to the proper brightness"""
+  now = datetime.now().time()
+  if (time(DAY_NIGHT_FROM, 0) <= now <= time(DAY_NIGHT_FROM, 1)) or (time(DAY_NIGHT_TO, 0) <= now <= time(DAY_NIGHT_TO, 1)):
+    if os.path.exists(memory_file):
+      os.remove(memory_file)
 
 def main():
   """Main function"""
   try:
     load_config()
+    reset_brightness()
+    set_state_errors = 0
     global MEMORY_UPDATED
     LEDS_ARE_OK = check_state()
     #initializing memory array
@@ -208,8 +220,8 @@ def main():
     response = requests.get(url_api, headers=headers).json()
     #check response
     if len(response) != len(regions_api_map):
-      print(f"Неверная длина ответа: {len(response)} != {len(regions)}")
-      raise ValueError(f"Неверная длина ответа: {len(response)} != {len(regions)}")
+      print(f"Неверная длина ответа: {len(response)} != {len(regions_api_map)}")
+      raise ValueError(f"Неверная длина ответа: {len(response)} != {len(regions_api_map)}")
     #saving new data to future save to file
     new_region_status = ""
     #logging what we've got for debug purpose if DEBUG is enabled in logger
@@ -224,26 +236,34 @@ def main():
       #if memory table is avaliable and some data changed
       elif MEMORY_UPDATED and status != memory[region_id] and LEDS_ARE_OK:
         if status == "A":
-          set_state(regions.get(regions_api_map[region_id]),RED)
+          if not set_state(regions.get(regions_api_map[region_id]),RED):
+            set_state_errors = 1
         elif status == "P":
-          set_state(regions.get(regions_api_map[region_id]),YELLOW)
+          if not set_state(regions.get(regions_api_map[region_id]),YELLOW):
+            set_state_errors = 1
         else:
-          set_state(regions.get(regions_api_map[region_id]),GREEN)
+          if not set_state(regions.get(regions_api_map[region_id]),GREEN):
+            set_state_errors = 1
         logging.info(f"{regions_api_map[region_id]} status changed to {status}")
         new_region_status += status
       #if memory not available - setting all zones to actual status
       elif not MEMORY_UPDATED or not LEDS_ARE_OK:
         if status == "A":
-          set_state(regions.get(regions_api_map[region_id]),RED)
+          if not set_state(regions.get(regions_api_map[region_id]),RED):
+            set_state_errors = 1
         elif status == "P":
-          set_state(regions.get(regions_api_map[region_id]),YELLOW)
+          if not set_state(regions.get(regions_api_map[region_id]),YELLOW):
+            set_state_errors = 1
         else:
-          set_state(regions.get(regions_api_map[region_id]),GREEN)
+          if not set_state(regions.get(regions_api_map[region_id]),GREEN):
+            set_state_errors = 1
         logging.info(f"{regions_api_map[region_id]} status changed to {status}")
         new_region_status += status
         delay.sleep(0.5)
     with open(memory_file, "w", encoding="utf-8") as f:
       f.write(new_region_status)
+    if set_state_errors != 0:
+      asyncio.run(send_to_telegram(f"set_state() errors appeared during the operations! Check logs!",f"🚒AlarmMap-Ukraine update script:"))
     logging.info(f"Actual data saved successfully to {os.path.join(Path(__file__).resolve().parent,'memory.json')}")
   except Exception as err:
     logging.error(f"main() global error: {err}")
